42dbfb56c45b28b010d8c7513ce652e336c5ad1b
safwaan41 <safwaan41@gmail.com>
2023-08-31 20:58:43 -0500

    PORTFOLIO DONE. WORKING ON CONTACT PAGE

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {BsInfoCircleFill} from 'react-icons/bs'
+import { BsInfoCircleFill } from 'react-icons/bs'
 import PageHeaderContent from "../../components/pageHeaderContent";
+import{Animate} from 'react-simple-animate'
 
 const Contact = () => {
     return (
@@ -10,7 +11,48 @@ const Contact = () => {
                 headerText='Contact Me'
                 icon={<BsInfoCircleFill size={40} />}
             />
-            
+            <div className="contact__content">
+                <Animate
+                    play
+                    duration={1}
+                    delay={0}
+                    start={{
+                        transform: 'translateX(-200px)'
+                    }}
+                    end={{
+                        transform: 'translateX(0px)'
+                    }}
+                >
+                    <h3 className="contact__content__header-text">
+                        Let's Talk
+                    </h3>
+
+                </Animate>
+                <Animate
+                    play
+                    duration={1}
+                    delay={0}
+                    start={{
+                        transform: 'translateX(200px)'
+                    }}
+                    end={{
+                        transform: 'translateX(0px)'
+                    }}
+                >
+                    <div className="contact__content__form">
+
+                        <div className="contact__content__form__controlswrapper">
+                            <div className="nameWrapper"></div>
+                            <div className="emailWrapper"></div>
+                            <div className="descriptionWrapper"></div>
+
+                        </div>
+
+                    </div>
+                </Animate>
+
+            </div>
+
         </section>
     )
 }
