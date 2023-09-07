c5bd6c0caf395f7071ddf77911f1e3320015d74b
safwaan41 <safwaan41@gmail.com>
2023-09-07 13:36:04 -0500

    navbar

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 import React, { useState } from "react";
 import { FaBars, FaReact } from 'react-icons/fa';
 import { HiX } from 'react-icons/hi'
@@ -41,10 +42,13 @@ const Navbar = () => {
     return (
         <div>
             <nav className="navbar">
+
                 <div className="navbar__container">
+
                     <Link to={'/'} className="navbar__container__logo">
                         <FaReact size={50} />
                     </Link>
+
                 </div>
                 <ul className={`navbar__container__menu ${toggleIcon ? "active" : ""}`}>
                     {
@@ -56,11 +60,17 @@ const Navbar = () => {
                             </li>
                         ))
                     }
+
                 </ul>
                 <div className="nav-icon" onClick={handleToggleIcon}>
-                    {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+                    {
+                        toggleIcon ? <HiX size={30} /> : <FaBars size={30} />
+                    }
+
                 </div>
+
             </nav>
+
         </div>
     )
 }
